9fb3c01db9e066fc4b868df9917a24114a5f6cae
L.Rossi <102166666+M-L-Rossi@users.noreply.github.com>
2022-11-15 10:45:24 -0300

    Add files via upload

--- php/update-og.php
+++ php/update-og.php
@@ -9,7 +9,15 @@ WHERE fecha = (SELECT MAX( fecha ) FROM estado);';
 $result = mysqli_query($connect, $query);
 
 while($row = mysqli_fetch_assoc($result)) {
-    echo "<h1 class='estado' id='temp'>  " . $row["temperatura"]. "°  " . $row["humedad"]. "% </h1>";
+    echo "
+    <div class='column has-text-centered'>
+        <h2 class='is-size-4 has-text-warning'>Temperatura Registrada</h2>
+        <h2 class='is-size-1'>" . $row["temperatura"]. " °C</h2>
+    </div>
+    <div class='column has-text-centered'>
+        <h2 class='is-size-4 has-text-warning'>Humedad Registrada</h2>
+        <h2 class='is-size-1'>" . $row["humedad"]. " %</h2>
+    </div></h1>";
 }
 
 
